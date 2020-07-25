b574e6aa234c0347c50885835bba050303e35aec
Quentin Madruga <qmadruga@uwo.ca>
2020-07-25 16:21:12 -0400

    added a quote of the day command that reaches out to TheySaidSo quote api

--- Command.js
+++ Command.js
@@ -1,4 +1,5 @@
 const CopyPasta = require("./CopyPasta.json");
+const https = require('https');
 
 exports.handleCommand = function(command){
     let cmd = command.content.substr(1);
@@ -15,6 +16,9 @@ exports.handleCommand = function(command){
             break;
         case "quote":
             quoteCommand(args, command);
+            break;
+        case "quoteOfTheDay":
+            quoteOfTheDay(args, command);
             break;
         default:
             command.channel.send("Unknown command");
@@ -45,4 +49,25 @@ function quoteCommand(args,cmd){
     }else{
         cmd.channel.send("No quote found");
     }
+}
+
+function quoteOfTheDay(args,cmd){
+    https.get("https://quotes.rest/qod?language=en", (response) => {
+        let data = '';
+
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        response.on('end', () => {
+            const quoteText = JSON.parse(data).contents.quotes[0].quote;
+            const author = JSON.parse(data).contents.quotes[0].author;
+            const quote = quoteText + " - " + author;
+
+            cmd.channel.send(quote);
+        });
+        response.on("error", (err) => {
+            console.log(err);
+        });
+    })
 }
