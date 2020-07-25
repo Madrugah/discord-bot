39182074c4968c9ec4d23665ad4e3c57e07e9c4a
Quentin Madruga <qmadruga@uwo.ca>
2020-07-25 13:04:49 -0400

    initial setup of bot

---
+++ bot.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () =>{
+    console.log("binky bonky " + client.user.tag);
+})
+client.login("NzM2NjI1NTIwMTgzMjE0MDgw.Xxxh-Q.QFoGMljcZ1PBLXNev131jNl7gtI");
