ba4680e779f263c6a10b795103452bb41b8fba03
Quentin Madruga <qmadruga@uwo.ca>
2020-07-25 15:05:47 -0400

    added help command and ping command to bot

--- bot.js
+++ bot.js
@@ -9,12 +9,57 @@ client.on('ready', () =>{
     console.log("Connected to servers: ");
     client.guilds.cache.forEach((guild) => {
         console.log(guild.name);
-        guild.channels.cache.forEach((channel) =>{
-            console.log(` - ${channel.name} ${channel.id}`);
-            //382892572424798219 swag chat
-        });
+        // guild.members.cache.forEach((mem) => console.log(mem.user));
     });
+
     let swagChat = client.channels.cache.get("382892572424798219");
-    swagChat.send("Binky Bonky");
+    // const att = new Discord.MessageAttachment("");
+    // swagChat.send(att);
 });
+
+client.on("message", (msg) =>{
+    if(msg.author == client.user) return;
+
+    if(msg.content.toLowerCase().includes("binky bonky")){
+        msg.channel.send("ayyy " + msg.author.toString() + " you cool!");
+        msg.react("🥰")
+    }
+
+    if(msg.content.startsWith("!")){
+        handleCommand(msg);
+    }
+    return;
+});
+
+function handleCommand(command){
+    let cmd = command.content.substr(1);
+    let splitCmd = cmd.split(" ");
+    let primary = splitCmd[0];
+    let args = splitCmd.slice(1);
+
+    switch(primary){
+        case "help":
+            helpCommand(args,command);
+            break;
+        case "ping":
+            pingCommand(args, command);
+            break;
+        default:
+            msg.channel.send("Unknown command");
+    }
+}
+function helpCommand(args, cmd){
+    if(args.length == 0){
+        cmd.channel.send("Get help!");
+    }else{
+        //handle help command
+    }
+}
+function pingCommand(args, cmd){
+    if(args.length > 0){
+        cmd.channel.send("Binky Bonky " + args[0] + "!");
+    }else{
+        cmd.channel.send("Binky Bonky!");
+    }
+}
 client.login("NzM2NjI1NTIwMTgzMjE0MDgw.Xxxh-Q.QFoGMljcZ1PBLXNev131jNl7gtI");
