02bd880dfc97feddac168bbb55219de6317f2550
Quentin Madruga <qmadruga@uwo.ca>
2020-07-25 13:37:17 -0400

    made bot send a message to a discord chat channel

--- bot.js
+++ bot.js
@@ -2,6 +2,19 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () =>{
-    console.log("binky bonky " + client.user.tag);
-})
+    console.log("Now connected");
+
+    client.user.setActivity("Runescape");
+
+    console.log("Connected to servers: ");
+    client.guilds.cache.forEach((guild) => {
+        console.log(guild.name);
+        guild.channels.cache.forEach((channel) =>{
+            console.log(` - ${channel.name} ${channel.id}`);
+            //382892572424798219 swag chat
+        });
+    });
+    let swagChat = client.channels.cache.get("382892572424798219");
+    swagChat.send("Binky Bonky");
+});
 client.login("NzM2NjI1NTIwMTgzMjE0MDgw.Xxxh-Q.QFoGMljcZ1PBLXNev131jNl7gtI");
